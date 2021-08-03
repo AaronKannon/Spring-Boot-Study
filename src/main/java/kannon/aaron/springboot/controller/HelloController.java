318a11d5a0284420e84ceb213295bd897520b6a4
AaronKannon <wellingtonbmoraes@hotmail.com>
2021-08-03 12:04:27 -0300

    Adding a print in the page

---
+++ src/main/java/kannon/aaron/springboot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package kannon.aaron.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloMessage() {
+        return "Hello, Digital Innovation One!";
+    }
+}
